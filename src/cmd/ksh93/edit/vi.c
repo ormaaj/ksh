158ac10fa814e7c2ce8c6566f6d50e758831e723
Martijn Dekker <martijn@inlv.org>
2022-11-04 12:38:05 +0000

    emacs, vi: Fix use of uninitialised variable (re: 161139d0)
    The uparrow variable was sometimes read without being initialised
    first, e.g. when using ^R in emacs. This intermittently resulted in
    executing an extra 'previous history line' command (e.g. up arrow).
    
    src/cmd/ksh93/edit/{emacs,vi}.c:
    - Eliminate uparrow variable by checking the command entered (c)
      directly. This also avoids executing this code from a few
      unrelated commands that happen to end up in this code path.
    - Code readability tweak: rename isallwhitespace() (a name I am to
      blame for, not pghvlaans) to blankline().
    - Avoid calling blankline() unnecessarily.
    
    Resolves: https://github.com/ksh93/ksh/issues/572

--- src/cmd/ksh93/edit/vi.c
+++ src/cmd/ksh93/edit/vi.c
@@ -175,7 +175,7 @@ typedef struct _vi_
 
 static const char paren_chars[] = "([{)]}";   /* for % command */
 
-static char	allwhitespace(Vi_t*);
+static char	blankline(Vi_t*);
 static void	cursor(Vi_t*, int);
 static void	del_line(Vi_t*,int);
 static int	getcount(Vi_t*,int);
@@ -448,7 +448,6 @@ static int cntlmode(Vi_t *vp)
 {
 	register int c;
 	register int i;
-	register int uparrow;
 	genchar tmp_u_space[MAXLINE];	/* temporary u_space */
 	genchar *real_u_space;		/* points to real u_space */
 	int tmp_u_column = INVALID;	/* temporary u_column */
@@ -613,7 +612,6 @@ static int cntlmode(Vi_t *vp)
 			}
 			save_v(vp);
 			cur_virt = INVALID;
-			uparrow = 0;
 			goto newhist;
 
 		case 'k':		/** get previous command **/
@@ -648,7 +646,6 @@ static int cntlmode(Vi_t *vp)
 			}
 			save_v(vp);
 			cur_virt = INVALID;
-			uparrow = 1;
 		newhist:
 			if(curhline!=histmax || cur_virt==INVALID)
 				hist_copy((char*)virtual, MAXLINE, curhline,-1);
@@ -676,13 +673,11 @@ static int cntlmode(Vi_t *vp)
 				vi_redraw((void*)vp);
 			}
 #endif /* SHOPT_EDPREDICT */
-			if(allwhitespace(vp))
-			{
-				if(uparrow && curhline != histmin)
-					ed_ungetchar(vp->ed,'k');
-				if(!uparrow && curhline != histmax)
-					ed_ungetchar(vp->ed,'j');
-			}
+			/* skip blank lines when going up/down in history */
+			if((c=='k' || c=='-') && curhline != histmin && blankline(vp))
+				ed_ungetchar(vp->ed,'k');
+			else if((c=='j' || c=='+') && curhline != histmax && blankline(vp))
+				ed_ungetchar(vp->ed,'j');
 			break;
 
 
@@ -705,7 +700,7 @@ static int cntlmode(Vi_t *vp)
 		case 'v':
 			if(vp->repeat_set==0)
 			{
-				if(allwhitespace(vp) || cur_virt == INVALID)
+				if(blankline(vp) || cur_virt == INVALID)
 				{
 					cur_virt = 0;
 					last_virt = cur_virt;
@@ -1295,7 +1290,7 @@ static void getline(register Vi_t* vp,register int mode)
 
 		case '\t':		/** command completion **/
 		{
-			if(allwhitespace(vp))
+			if(blankline(vp))
 			{
 				ed_ringbell();
 				break;
@@ -2318,7 +2313,7 @@ addin:
 	case '\\':		/** do file name completion in place **/
 	case '=':		/** list file name expansions **/
 	{
-		if(cur_virt == INVALID || allwhitespace(vp))
+		if(cur_virt == INVALID || blankline(vp))
 			return(BAD);
 		/* FALLTHROUGH */
 		save_v(vp);
@@ -2672,9 +2667,9 @@ yankeol:
 #endif	/* SHOPT_MULTIBYTE */
 
 /*
- * determine if a line is entirely blank
+ * determine if the command line is blank (empty or all whitespace)
  */
-static char allwhitespace(register Vi_t *vp)
+static char blankline(Vi_t *vp)
 {
 	int x;
 	for(x=0; x <= cur_virt; x++)

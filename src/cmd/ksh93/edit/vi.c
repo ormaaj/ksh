fb64d620587a34d212da5b37340b9a1302f10312
pghvlaans <kvngncrlsn@gmail.com>
2023-01-08 20:46:53 +0900

    Bring in-word completion in line with the book (#580)
    In both editing modes, using = or * completion from within a word
    fails to replace the word; any trailing characters are printed at
    the end of the completion. In addition, no characters under or
    beyond the cursor (beyond the cursor in vi mode) are taken into
    account when constructing the list of options. As an example, in
    emacs mode with the cursor on i, $ ls foi<^M-=> can produce the
    following completion menu:
    
    1) foible
    2) foil
    3) foo.bar
    
    According to Bolsky and Korn, pp. 105-106 and 114-115, completion
    characters are meant to be appended to the word. This implies that
    the completion operation takes place on the entire word, as it does
    in pdksh, oksh and mksh. In the example above, full-word completion
    would result in a menu of:
    
    1) foible
    2) foil
    
    src/cmd/ksh93/edit/vi.c:
    - Save cur_virt and increment until either virtual[cur_virt] or
      virtual[cur_virt+1] is non-alphanumeric. Move the cursor back to
      the original position if completion fails or in case of a
      completion menu.
    
    src/cmd/ksh93/edit/emacs.c:
    - Save cur and increment until out[cur] is non-alphanumeric. Move
      the cursor back to the original position if completion fails or
      in case of = completion with a menu, no count set and = as the
      most-recently entered key.

--- src/cmd/ksh93/edit/vi.c
+++ src/cmd/ksh93/edit/vi.c
@@ -2538,6 +2538,7 @@ static int textmod(register Vi_t *vp,register int c, int mode)
 	register genchar *p = vp->lastline;
 	register int trepeat = vp->repeat;
 	genchar *savep;
+	int savecur;
 	int ch;
 
 	if(mode && (fold(vp->lastmotion)=='F' || fold(vp->lastmotion)=='T')) 
@@ -2583,6 +2584,9 @@ addin:
 		++last_virt;
 		mode = cur_virt-1;
 		virtual[last_virt] = 0;
+		savecur = cur_virt;
+		while(isalph(cur_virt) && isalph(cur_virt+1))
+			cur_virt++;
 		ch = c;
 		if(mode>=0 && c=='\\' && virtual[mode+1]=='/')
 			c = '=';
@@ -2595,6 +2599,7 @@ addin:
 #endif /* SHOPT_MULTIBYTE */
 		if(ed_expand(vp->ed,(char*)virtual, &cur_virt, &last_virt, ch, vp->repeat_set?vp->repeat:-1)<0)
 		{
+			cur_virt = savecur;
 			if(vp->ed->e_tabcount)
 			{
 				vp->ed->e_tabcount=2;
@@ -2608,6 +2613,7 @@ addin:
 		else if(vp->ed->e_nlist!=0 && !vp->repeat_set)
 		{
 			last_virt = i;
+			cur_virt = savecur;
 			vi_redraw((void*)vp);
 			return(GOOD);
 		}

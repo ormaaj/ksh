c5c839257eebfcc8af0437d75df50fdcefb79649
Martijn Dekker <martijn@inlv.org>
2024-03-23 15:10:11 +0000

    another round of minor tweaks and cleanups
    Notable changes:
    
    Many files:
    - Eliminate redundant typecasts in free(3) calls
    
    src/lib/libast/cdt/dttree.c:
    - Remove unused Dttree global variable
    
    src/lib/libast/features/lib:
    - Remove unused tests for catopen(3), catclose(3), catgets(3)

--- src/cmd/ksh93/tests/builtins.sh
+++ src/cmd/ksh93/tests/builtins.sh
@@ -1670,7 +1670,7 @@ exp='\\ \\\\'
 	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
 
 # ======
-case $(PATH=/opt/ast/bin:$PATH; exec cat '--???SECTION' 2>&1) in
+case $(PATH=/opt/ast/bin:$PATH; exec cat '--???SECTION' </dev/null 2>&1) in
 1)	err_exit "'exec' runs non-external command" ;;
 esac
 
